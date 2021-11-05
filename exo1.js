7cde6d074a448b35d2cc538daf56b84b1692a1f8
audemar <audemarsadish@gmail.com>
2021-11-05 11:50:09 +0100

    exo1 terminé

---
+++ exo1.js
@@ -0,0 +1,6 @@
+let firstNumber = document.getElementById("firstNumber").value;
+let secondNumber = document.getElementById("secondNumber").value;
+let sum = firstNumber * secondNumber; document.getElementById("sum").value = sum;
+function number(firstNumber , secondNumber) {
+    return firstNumber * secondNumber;
+  }
